7273cfe2de615018cf90f0f0edabd511b8047153
miskaldukhi <miskaldukhi@gmail.com>
2025-02-18 19:53:55 +0300

    test

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+
+    val adminRole = UserRole.ADMIN
+    val editorRole = UserRole.EDITOR
+    val viewerRole = UserRole.VIEWER
+    println("Role: ${adminRole}")
+    println("Role: ${editorRole}")
+    println("Role: ${viewerRole}")
 }
+enum class UserRole (val accessLevel:Int) {
+    ADMIN(1),
+    EDITOR(2),
+    VIEWER(3)
+}
+fun u
