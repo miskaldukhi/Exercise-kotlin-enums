6adb79052b11bb972edda5141960bb0b0e0b66b6
miskaldukhi <miskaldukhi@gmail.com>
2025-02-20 14:21:31 +0300

    enum done

--- src/Main.kt
+++ src/Main.kt
@@ -3,13 +3,24 @@ fun main() {
     val adminRole = UserRole.ADMIN
     val editorRole = UserRole.EDITOR
     val viewerRole = UserRole.VIEWER
+
     println("Role: ${adminRole}")
     println("Role: ${editorRole}")
     println("Role: ${viewerRole}")
+
+    println("\nAll roles and their access levels:")
+    for (role in UserRole.values()) {
+        println("${role.name}: Access Level = ${role.accessLevel}")
+    }
+
 }
 enum class UserRole (val accessLevel:Int) {
     ADMIN(1),
     EDITOR(2),
     VIEWER(3)
 }
-fun u
+fun checkAccessLevel(userRole: UserRole, requiredAccessLevel: Int): Boolean {
+    return userRole.accessLevel >= requiredAccessLevel
+
+
+}
